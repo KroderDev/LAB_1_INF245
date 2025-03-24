dd452743ffe836b07c10c845ec56b0c47ead7cb2
KroderDev <142845401+KroderDev@users.noreply.github.com>
2025-03-24 14:17:15 -0300

    Convertir a Decimal
    Binario > suma de potencias de 2

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # METAL GEAR SOLID 1010: BINARY SNAKE
- 
-### Contacto
+
+## Documentación
+
+### Función: `convertir_a_decimal`
+Convierte a decimal el número ingresado de base indicada aplicando algorítmos de conversión.
+#### **Parámetros**
+- num (`int` o `str`): Número a convertir.
+- base (`str`): La base del número que será convertido.
+    - `'BIN'` : Binario.
+    - `'OCT'` : Octal.
+    - `'HEX'` : Hexadecimal.
+#### **Retorna**
+- `int`: El número de entrada convertido a decimal.
+
+### Clase: `Tablero`
+
+## Contacto
 * **Sebastián Richiardi**, Rol; 20203055-2, Paralelo: 201
 * **Gabriel Alejandro Toro Varela**, Rol; 202204557-4, Paralelo: 201
--- t1.py
+++ t1.py
@@ -1,5 +1,28 @@
 import random
 
+def convertir_a_decimal(num, base):
+    try:
+        match base:
+            case 'BIN':
+                decimal = 0
+                power = 0
+                for i in reversed(str(num)):
+                    if i == '1': # Si el bit es 1 sumamos 2**(portencia) al decimal
+                        decimal += 2**power
+                    elif i != '0': # Manejo de error
+                        raise('El número binario es inválido.')
+                    
+                    power+=1 # incrementar la potencia
+                return decimal
+            case 'OCT':
+                pass
+            case 'HEX':
+                pass
+            case _:
+                return None
+    except:
+        raise ValueError('Ocurrió un error inesperado')
+
 class Tablero:
     def __init__(self, largo, guardias):
         self.largo = largo
