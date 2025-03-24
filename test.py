d807fc4f1e7f57de1abeba04fe8f364208fc6cc1
KroderDev <142845401+KroderDev@users.noreply.github.com>
2025-03-24 14:17:24 -0300

    Test

---
+++ test.py
@@ -0,0 +1,22 @@
+from t1 import convertir_a_decimal
+
+print(convertir_a_decimal(1010, 'BIN'))
+print(convertir_a_decimal(1011, 'BIN'))
+print(convertir_a_decimal(101010, 'BIN'))
+print(convertir_a_decimal(0, 'BIN'))
+print(convertir_a_decimal(1, 'BIN'))
+print(convertir_a_decimal('11111111', 'BIN'))
+
+print(convertir_a_decimal(10, 'OCT')) # 8
+print(convertir_a_decimal(17, 'OCT')) # 15
+print(convertir_a_decimal(123, 'OCT')) # 83
+print(convertir_a_decimal('777', 'OCT')) # 511
+print(convertir_a_decimal(0, 'OCT')) # 0
+print(convertir_a_decimal(77, 'OCT')) # 63
+
+print(convertir_a_decimal('A', 'HEX')) # 10
+print(convertir_a_decimal('1F', 'HEX')) # 31
+print(convertir_a_decimal('ABC', 'HEX')) # 2748
+print(convertir_a_decimal('FF', 'HEX')) # 255
+print(convertir_a_decimal('0', 'HEX')) # 0
+print(convertir_a_decimal('123456', 'HEX')) # 1193046
